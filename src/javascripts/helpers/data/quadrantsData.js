343ec7e86c2a01286f26482dfd31a2b614623913
Liz <liz@Liz-s-Mac.local>
2020-09-05 20:40:22 -0500

    style quadrants and update data

--- src/javascripts/helpers/data/quadrantsData.js
+++ src/javascripts/helpers/data/quadrantsData.js
@@ -3,25 +3,33 @@ const quadrantData = [
     activity: 'Eat',
     score: 100,
     button1: 'Healthy',
-    button2: 'Unhealthy'
+    button2: 'Unhealthy',
+    button1Value: 10,
+    button2Value: -3
   },
   {
     activity: 'Play',
     score: 50,
-    button1: 'Best time ever',
-    button2: 'Have an OK time'
+    button1: 'Fun',
+    button2: 'Content',
+    button1Value: 50,
+    button2Value: 2
   },
   {
     activity: 'Fight',
     score: 100,
     button1: 'Run',
-    button2: 'Fight'
+    button2: 'Fight',
+    button1Value: 1,
+    button2Value: -10
   },
   {
     activity: 'Sleep',
     score: 100,
     button1: 'Nap',
-    button2: 'Sleep'
+    button2: 'Sleep',
+    button1Value: 50,
+    button2Value: 60
   }
 ];
 
